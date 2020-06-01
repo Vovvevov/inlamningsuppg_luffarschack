7e2ffd1cb2b3cbdf31e940cb89b8c532f7558887
Vovvevov <ludvigdahlstedt@gmail.com>
2020-06-01 16:14:04 +0200

    Upload of the finished code

---
+++ main.js
@@ -0,0 +1,314 @@
+const playingGridDiv = document.querySelector('#playing_grid');
+const playerToMoveH2 = document.querySelector('#player_to_move > h2');
+const totalNumberOfMovesH2 = document.querySelector('#total_number_of_moves > h2');
+const header = document.querySelector('header');
+let fiveInARow = false;
+let playerWhoHasWon = '';
+let playerOneHasRingsOrXs = '';
+
+const playingGridArray = [];
+const arrayOfDivs = [];
+
+let sideLengthOfPlayingGrid = prompt("Hur många rutor brett och högt vill du att spelbrädet ska vara?")
+
+let activePlayer = 'Spelare 1'
+let numberOfMovesMade = 0
+
+const h2WithInfoAboutWhichPlayerHasRings = document.createElement('h2');
+header.appendChild(h2WithInfoAboutWhichPlayerHasRings);
+
+function toggleActivePlayer(){
+
+        if(activePlayer === 'Spelare 1'){
+            activePlayer = 'Spelare 2';
+        }
+        else if(activePlayer === 'Spelare 2'){
+            activePlayer = 'Spelare 1';
+    }
+}
+
+function createRandomNumberZeroOrOne(){
+    let randomNumberRaw = Math.random();
+    let randomNumberZeroOrOne;
+
+    if(randomNumberRaw < 0.5){
+        randomNumberZeroOrOne = 0;
+    }
+    else{
+        randomNumberZeroOrOne = 1;
+    }
+    return randomNumberZeroOrOne;
+}
+
+function decideWhichPlayerHasRingsOrXsRespectively(zeroOrOne){
+    playerOneHasRingsOrXs;
+    
+    if(randomNumberZeroOrOne === 0){
+        playerOneHasRingsOrXs = 'rings';
+    }
+    else if(randomNumberZeroOrOne === 1){
+        playerOneHasRingsOrXs = 'xs';
+    }
+
+    return playerOneHasRingsOrXs;
+}
+
+function displayWhichPlayerHasRingsOrXsRespectively(playerOnesMarkType){
+    if(playerOneHasRingsOrXs === 'rings'){
+        h2WithInfoAboutWhichPlayerHasRings.textContent = 'Spelare 1 har slumpats till att spela med ringar och spelare 2 med kryss.';
+    }
+    else if(playerOneHasRingsOrXs === 'xs'){
+        h2WithInfoAboutWhichPlayerHasRings.textContent = 'Spelare 1 har slumpats till att spela med kryss och spelare 2 med ringar.';
+    }
+}
+
+randomNumberZeroOrOne = createRandomNumberZeroOrOne();
+playerOneHasRingsOrXs = decideWhichPlayerHasRingsOrXsRespectively(randomNumberZeroOrOne);
+displayWhichPlayerHasRingsOrXsRespectively(playerOneHasRingsOrXs);
+
+function activePlayerDisplay(){
+playerToMoveH2.textContent = `${activePlayer}'s tur!`;
+}
+
+function totalNumberOfMovesCount(){
+    numberOfMovesMade += 1;
+}
+
+function totalNumberOfMovesDisplay(){
+    totalNumberOfMovesH2.textContent = `Totalt antal drag gjorda: ${numberOfMovesMade}`;
+}
+
+
+
+function decideIf5InARowHor(row, col){
+    let loopStart = col - 4;
+
+    if (loopStart < 0){
+        loopStart = 0
+    }
+
+    let loopEnd = col + 5;
+
+    if (loopEnd > sideLengthOfPlayingGrid - 1){
+        loopEnd = sideLengthOfPlayingGrid - 1
+    }
+
+    let checkedString = ""
+
+    for(let i = loopStart; i < loopEnd; i++){
+
+        checkedString = checkedString + playingGridArray[row][i].textContent;
+
+        if(checkedString.includes('OOOOO')){
+
+            fiveInARow = true;
+
+            if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;
+        }
+
+        else if(checkedString.includes('XXXXX')){
+
+            fiveInARow = true;
+    
+            if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;  
+        }
+        //console.log(checkedString)
+        //console.log(checkedString.includes("OOOOO"))
+    }
+}
+
+function decideIf5InARowVert(col, row){
+    let loopStart = row - 4;
+
+    if (loopStart < 0){
+        loopStart = 0
+    }
+
+    let loopEnd = row + 5;
+
+    if (loopEnd > sideLengthOfPlayingGrid - 1){
+        loopEnd = sideLengthOfPlayingGrid - 1
+    }
+
+    let checkedString = ""
+
+    for(let i = loopStart; i < loopEnd; i++){
+
+        checkedString = checkedString + playingGridArray[i][col].textContent;
+
+        if(checkedString.includes('OOOOO')){
+
+            fiveInARow = true;
+
+            if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;
+        }
+
+        else if(checkedString.includes('XXXXX')){
+
+            fiveInARow = true;
+    
+            if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;  
+        }
+        //console.log(checkedString)
+        //console.log(checkedString.includes("OOOOO"))
+    }
+}
+
+function decideIf5InARowDiagDown(col, row){
+    let loopStartRow = row -4;
+    let loopStartCol = col -4;
+
+    if (loopStartRow < 0){
+        loopStartRow = 0;
+    }
+    if (loopStartCol < 0){
+        loopStartCol = 0;
+    }
+
+    let loopEndRow = row + 5;
+    let loopEndCol = col + 5;
+
+    if (loopEndRow > sideLengthOfPlayingGrid - 1){
+        loopEndRow = sideLengthOfPlayingGrid - 1
+    }
+    if (loopEndCol > sideLengthOfPlayingGrid -1 ){
+        loopEndCol = sideLengthOfPlayingGrid - 1
+    }
+
+    let checkedString = ""
+
+    for (let i = loopStartCol, j = loopStartRow; i < loopEndCol, j < loopEndRow; i++, j++){
+
+        checkedString = checkedString + playingGridArray[i][j].textContent;
+        //console.log(checkedString);
+
+        if(checkedString.includes('OOOOO')){
+
+            fiveInARow = true;
+
+            if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;
+        }
+
+        else if(checkedString.includes('XXXXX')){
+
+            fiveInARow = true;
+    
+            if(playerOneHasRingsOrXs === 'xs'){
+                playerWhoHasWon = 'Spelare 1';
+            }
+            else if(playerOneHasRingsOrXs === 'rings'){
+                playerWhoHasWon = 'Spelare 2';
+            }
+
+            playerToMoveH2.textContent = `${playerWhoHasWon} har vunnit denna omgång!`;  
+        }
+        //console.log(checkedString)
+        //console.log(checkedString.includes("OOOOO"))
+    }
+}
+
+for(let rowNr = 0; rowNr < sideLengthOfPlayingGrid; rowNr++){
+    playingGridArray[rowNr] = []
+
+    for(let columnNr = 0; columnNr < sideLengthOfPlayingGrid; columnNr++){
+
+        if(columnNr % sideLengthOfPlayingGrid === 0){
+            arrayOfDivs[rowNr] = document.createElement('div');
+            playingGridDiv.appendChild(arrayOfDivs[rowNr]);
+        }
+       
+        playingGridArray[rowNr][columnNr] = document.createElement('button');
+        playingGridArray[rowNr][columnNr].id = columnNr + ' ' + rowNr;
+
+        playingGridArray[rowNr][columnNr].addEventListener('click', function(event){
+
+            if(fiveInARow === false){
+
+                if(!(event.currentTarget.classList.contains('clicked'))){
+
+                    event.currentTarget.classList.add('clicked');
+
+                    if(activePlayer === 'Spelare 1'){
+                        event.currentTarget.classList.add('clicked_by_player_1');
+
+                        if(playerOneHasRingsOrXs === 'rings'){
+                            event.currentTarget.textContent = 'O';
+                        }
+                        else if(playerOneHasRingsOrXs === 'xs'){
+                            event.currentTarget.textContent = 'X';
+                        }
+                    }
+
+                    else if(activePlayer === 'Spelare 2'){
+                        event.currentTarget.classList.add('clicked_by_player_2');
+                    
+                        if(playerOneHasRingsOrXs === 'rings'){
+                            event.currentTarget.textContent = 'X';
+                        }
+
+                        else if(playerOneHasRingsOrXs === 'xs'){
+                            event.currentTarget.textContent = 'O';
+                        }
+                    }
+                }
+
+                toggleActivePlayer();
+                totalNumberOfMovesCount();
+                totalNumberOfMovesDisplay();
+                activePlayerDisplay();
+                decideIf5InARowHor(rowNr, columnNr);
+                decideIf5InARowVert(columnNr, rowNr);
+                decideIf5InARowDiagDown(columnNr, rowNr);
+
+                    //console.log(rowNr)
+                    //console.log(columnNr)
+                    //console.log(playerOneHasRingsOrXs)
+                    //console.log(playerWhoHasWon)
+                    
+            }
+        })  
+
+        arrayOfDivs[rowNr].appendChild(playingGridArray[rowNr][columnNr]);
+        playingGridArray[rowNr][columnNr].textContent = " ";
+
+    }
+    //console.log(checkedString)
+    //console.log(fiveInARow)
+    //console.log(playerOneHasRingsOrXs)
+    
+}
